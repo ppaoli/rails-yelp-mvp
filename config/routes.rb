ae7c5b1b7bd812f2392861f1771d98118f2d0f62
philippe paoli <philippe.paoli11@gmail.com>
2022-11-16 16:02:38 +0000

    build a restaurant app with reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating restaurants..."
+sushimambo ={name: "Sushi Mambo", address: "Holborn", category: "japanese"}
+vapiano = {name: "Vapiano", address: "Shoreditch", category: "italian"}
+friestars = {name: "Friestar", address: "Brussels", category: "belgian"}
+chopsticks = {name: "Chopsticks", address: "Paris", category: "chinese"}
+entrecote = {name: "Entrecote", address: "Soho", category: "french"}
+
+[sushimambo, vapiano, friestars, chopsticks, entrecote].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
